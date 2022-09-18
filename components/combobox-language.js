bfc91f317a490f8ddc275a51561106e16c58cdef
kim-duyen-frontend <ntduyen255@gmail.com>
2022-09-18 21:53:29 +0700

    add content page login

---
+++ components/combobox-language.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ComboboxLanguage = () => {
+    return (
+        <select>
+            <option value="Tiếng Việt">Tiếng Việt</option>
+            <option value="Tiếng Anh">Tiếng Anh</option>
+        </select>
+    );
+};
+
+export default ComboboxLanguage;
--- pages/auth/login/index.js
+++ pages/auth/login/index.js
@@ -1,9 +1,36 @@
+import Image from 'next/image';
 import React from 'react';
+import HeaderLogin from '../../../features/header';
+import styles from "../../../styles/login.module.scss";
 
 const LoginPage = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className={styles.loginPage}>
+            <HeaderLogin />
+            <div className={styles.logo}>
+                <Image priority src="/images/logo-login.png" layout="fill" objectFit="contain" />
+            </div>
+            <h2 className={styles.title}>Welcome Back</h2>
+            <div className={styles.container}>
+                <div className={styles.form}>
+                    <input
+                        className={styles.inputEmail}
+                        placeholder="Email..."
+                        type="email"
+                    />
+                    <input
+                        className={styles.inputPassword}
+                        placeholder="Password"
+                        type="password"
+                    />
+                    <button
+                        className={styles.btnlogin}
+                        disabled="true"
+                    >
+                        Log in
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
